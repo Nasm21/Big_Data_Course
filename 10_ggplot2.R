0e8a925c1c59e39d6fe0c9b1be96563d005190f8
Nasm21 <nataliasantamaria9@gmail.com>
2024-01-14 19:03:33 -0500

    topic course

---
+++ 10_ggplot2.R
@@ -0,0 +1,91 @@
+library(dplyr)
+library(ggplot2)
+library(dslabs)
+
+#https://r-graph-gallery.com/ggplot2-package.html
+#Funciona por capas, siendo las más importantes:
+#data = dataset
+#Aesthetic mapping = Propiedades visuales del gráfico aes()
+#Geometry = geom_X where X is the name of the geometry. 
+#Some examples include geom_point, geom_bar, and geom_histogram.
+#Facets = Permite dividir tu gráfico en subgráficos basados en una 
+#o más variables
+#Statistics = se utilizan para resumir los datos antes de graficarlos.
+#En muchos casos, ggplot2 automáticamente elige una estadística 
+#apropiada
+#Coordinates = Especifica el sistema de coordenadas utilizado en el gráfico.
+#Theme = Controla la apariencia general del gráfico, como los colores, 
+#tamaños de fuente, etc
+
+data("murders")
+ggplot(data = murders)
+ggplot(data=murders,aes(x = population/10^6, y = total))+
+  geom_point()
+
+murders %>% ggplot(aes(x = population/10^6, y = total)) + 
+  geom_point()
+
+murders %>% ggplot() + 
+  geom_point(aes(x = population/10^6, y = total))
+
+murders %>% ggplot(aes(x = population/10^6, 
+                       y = total,color=region,shape=region)) + 
+  geom_point()+xlab("Populations in millions (log scale)") + 
+  ylab("Total number of murders (log scale)") +
+  ggtitle("US Gun Murders in 2010")+ 
+  scale_x_continuous(trans = "log10") +
+  scale_y_continuous(trans = "log10") + 
+  facet_wrap(~ region, nrow = 2)
+
+p<- murders %>% ggplot(aes(x = population/10^6, 
+                           y = total,color=region)) + 
+  geom_point(aes(shape=region))+xlab("Populations in millions (log scale)") + 
+  ylab("Total number of murders (log scale)") +
+  ggtitle("US Gun Murders in 2010")+ 
+  scale_x_continuous(trans = "log10") +
+  scale_y_continuous(trans = "log10") 
+p
+
+install.packages("ggthemes")
+library(ggthemes)
+p + theme_economist()
+
+
+#Diagrama de barras
+ murders %>%
+  count(region) %>%
+  mutate(proportion = n/sum(n)) %>%
+   ggplot(aes(region, proportion,fill = region)) + 
+   geom_bar(stat = "identity")+
+   geom_smooth()
+
+#Diagrama circular
+murders %>%
+  count(region) %>%
+  mutate(proportion = n/sum(n)) %>%
+  ggplot(aes(x="", proportion,fill = region)) + 
+  geom_bar(stat = "identity",width = 1) +
+  coord_polar(theta = "y") 
+
+# Facetas
+murders %>%
+  count(region) %>%
+  mutate(proportion = n/sum(n)) %>% 
+  ggplot(aes(region, proportion,fill = region)) + 
+  geom_bar(stat = "identity") + 
+  facet_wrap(~ region, nrow = 2)
+
+#Varios gráficos
+
+x <- log10(murders$population)
+y <- murders$total
+
+p1 <- data.frame(x = x, y = y) %>% 
+  ggplot(aes(x,y)) +
+  geom_point()
+p2 <- data.frame(x = x) %>%
+  ggplot(aes(x = x)) +
+  geom_histogram()
+
+library(gridExtra)
+grid.arrange(p1, p2, ncol = 2)
